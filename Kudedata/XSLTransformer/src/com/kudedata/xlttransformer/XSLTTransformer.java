414b1b12b0e1b12bd1775bb37bd311a0936c9474
gechevarria <gorka.echevarria@tecnalia.com>
2015-02-02 10:19:18 +0100

    Kudedata v4.0

--- Kudedata/XSLTransformer/src/com/kudedata/xlttransformer/XSLTTransformer.java
+++ Kudedata/XSLTransformer/src/com/kudedata/xlttransformer/XSLTTransformer.java
@@ -12,13 +12,16 @@ import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
 public class XSLTTransformer {
+	private static String EDIMessage = "invoic";
+	
+	
 	public static void main(String[] args) throws IOException, URISyntaxException, TransformerException {
         TransformerFactory factory = TransformerFactory.newInstance();
-        Source xslt = new StreamSource(new File("D:/Projects/KUDETADA/eclipse-standard-luna-R-win32-x86_64/workspace/XSLTransformer/xslts/orders.xslt"));
+        Source xslt = new StreamSource(new File("D:/Projects/KUDETADA/eclipse-standard-luna-R-win32-x86_64/workspace/XSLTransformer/xslts/"+EDIMessage+".xslt"));
         Transformer transformer = factory.newTransformer(xslt);
 
-        Source text = new StreamSource(new File("D:/Projects/KUDETADA/eclipse-standard-luna-R-win32-x86_64/workspace/XSLTransformer/xml/order.xml"));
-        transformer.transform(text, new StreamResult(new File("D:/Projects/KUDETADA/eclipse-standard-luna-R-win32-x86_64/workspace/XSLTransformer/html/order.html")));
+        Source text = new StreamSource(new File("D:/Projects/KUDETADA/eclipse-standard-luna-R-win32-x86_64/workspace/XSLTransformer/xml/"+EDIMessage+".xml"));
+        transformer.transform(text, new StreamResult(new File("D:/Projects/KUDETADA/eclipse-standard-luna-R-win32-x86_64/workspace/XSLTransformer/html/"+EDIMessage+".html")));
     }
 
 }
